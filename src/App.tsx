1868de280c86a47e38d458407d2d688e7f8ca534
Daniel <kcomtdaniel@gmail.com>
2024-06-05 18:36:31 -0500

    fixed responsive styles and light/dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { HfInference } from "@huggingface/inference";
 import { useEffect, useState } from "react";
-import { PacmanLoader } from "react-spinners";
+import { DotLoader, PacmanLoader, PropagateLoader } from "react-spinners";
 
 function App() {
   const HF_TOKEN = import.meta.env.VITE_API_KEY;
@@ -28,49 +28,50 @@ function App() {
   }, [inputText]);
 
   return (
-    <main className="font-poppins w-full h-full py-24 md:py-18 lg:py-32 xl:py-48 bg-gray-100 dark:bg-gray-800">
-      <div className="container px-4 md:px-6">
-        <div className="flex flex-col items-center justify-center space-y-6 text-center mb-8">
-          <h1 className="dark:text-neutral-100 text-slate-800 text-4xl font-bold tracking-tighter sm:text-3xl md:text-6xl lg:text-7xl">
+    <main
+      className={`font-poppins ${
+        imageData ? "h-full" : "h-dvh "
+      } flex flex-col items-center justify-center  bg-gray-200 dark:bg-gray-800 py-10 md:py-18 lg:py-16 xl:py-20`}
+    >
+      <div className="container  max-w-[360px] sm:max-w-[580px]  md:max-w-[768px] flex flex-col items-center justify-center px-4 md:px-6 dark:bg-slate-900 bg-slate-300 rounded-xl p-4">
+        <div className="mb-8">
+          <h1 className="dark:text-neutral-100 text-slate-800 text-4xl font-bold tracking-tighter sm:text-3xl md:text-6xl lg:text-7xl text-center">
             Image Generator
           </h1>
-          <h3 className="dark:text-neutral-100 text-slate-800  tracking-tighter">
-            por @dansaround.dev
+          <h3 className="dark:text-neutral-100 md:text-xl text-slate-800 italic tracking-tighter text-center my-6 sm: text-lg">
+            by @dansaround.dev
           </h3>
-          <p className="dark:text-neutral-300 text-slate-700">
-            Escribe algo para poder generar una imagen, por ejemplo
+          <p className="dark:text-neutral-300 text-slate-700 my-8">
+            The following project was created using Huggingface.js Inference API
+            ! Feel free to try out this image generator by writing something to
+            generate an image, for example:
           </p>
-          <span className="dark:text-neutral-400 text-slate-700 text-left text-sm max-w-72 italic ">
+          <span className="dark:text-neutral-400 text-slate-700 text-sm max-w-72 italic text-left">
             "award winning high resolution photo of a giant
             tortoise/((ladybird)) hybrid, [trending on artstation]"
           </span>
         </div>
-        <div>
+        <div className="flex items-center justify-center mb-8 w-full bg-slate-600 rounded-md p-2">
           <input
             value={inputText}
             onChange={(e) => {
               setInputText(e.target.value);
             }}
-            className="px-4 py-2"
+            className="pl-4 py-2 rounded-md md:w-2/5 w-44"
             type="text"
           />
           <button
             onClick={createImage}
-            className=" ml-4 inline-flex px-10 h-8 items-center justify-around rounded-md bg-gray-900 text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+            disabled={isLoading}
+            className=" ml-4 px-4 h-8 items-center justify-around rounded-md bg-gray-900 text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300 "
           >
-            {!isLoading ? (
-              "Generar Imagen"
-            ) : (
-              <>
-                <span className="mx-3">Generando Imagen </span>
-                <PacmanLoader color="#36d7b7" size={15} />
-              </>
-            )}
+            {!isLoading ? "Create" : <DotLoader color="#36d7b7" size={20} />}
           </button>
         </div>
-        <div className="max-w-96 flex flex-col justify-center items-center fit-content ">
+        <div className="max-w-68 flex flex-col justify-center items-center fit-content">
           {imageData && (
             <img
+              className="max-h-[650px]"
               src={URL.createObjectURL(imageData)}
               alt="Generated AI Image with the prompt"
             />
